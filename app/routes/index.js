87db5ada07ff5cc7b44fbd4ebfea35fdb955c864
Bogdan Lishchynskiy <lishchynskiy91@gmail.com>
2017-09-27 16:11:44 +0300

    new change

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
  
  router.get('/', function(req, res) {
   res.send(`
-      <h1>ІВАН КОТЛЯРЕВСЬКИЙ</h1>
-      <h2><b>поема</h2>
-  `);
+  	<h1>Іван Котляревський</h1>
+  	<h2>Поема</h2>
+  	`);
 });
 
 module.exports = router;
